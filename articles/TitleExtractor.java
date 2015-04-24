d9d107ae11a5fc0a1e5b601dd248ec8971c29d73
Milan van der Meer <milanvdm@hotmail.com>
2015-04-24 14:30:58 +0800

    Started articles extraction

---
+++ articles/TitleExtractor.java
@@ -0,0 +1,64 @@
+package articles;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class TitleExtractor {
+
+	public static void main(String [] args)
+	{
+		String path = askInput();
+		File file = new File(path);
+		
+		try {
+			
+			PrintWriter out = new PrintWriter("titles.txt");	
+			Parser parser = new Parser(file);
+			
+			while(parser.hasNext()) {
+				Page page = parser.next();
+				String title = page.title;
+				
+				out.println(title);
+				
+			}
+			
+			out.close();
+			parser.close();
+			
+			
+		} catch (IOException e) {
+			System.out.println("It broke.");
+		}
+		
+		
+	}
+
+	private static String askInput() {
+		
+		//  prompt the user to enter the path
+		System.out.print("Enter the Simple English Wikipedia corpus: ");
+
+		//  open up standard input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String path = null;
+
+		//  read the path from the command-line; need to use try/catch with the
+		//  readLine() method
+		try {
+			path = br.readLine();
+		} catch (IOException ioe) {
+			System.out.println("IO error trying to read your path!");
+			System.exit(1);
+		}
+		return path;
+	}
+	
+	
+
+
+}
---
+++ main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Main {
+
+	private static String pathWiki = "C:\\Users\\Milan\\Desktop\\School\\CUHK\\AI\\Paper\\LanguageGenetics\\wikipedia_pos.txt";
+
+	public static void main(String args[]) throws IOException { 
+
+		
+	}
+
+	private static File askWikiFile() {
+		return new File(pathWiki);
+	}
+
+}
+
